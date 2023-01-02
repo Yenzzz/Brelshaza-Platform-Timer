67faa70359734a56dabb73b06ad0433c8453337b
Ynnn <99371443+Yenzzz@users.noreply.github.com>
2023-01-01 17:03:46 -0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,10 +10,97 @@ tds.forEach(function (element) {
   });
 });
 
+// ================== North Meteor ===============================================
+
+const northMeteorButton = document.querySelector(".north-meteor");
+northMeteorButton.addEventListener("click", function () {
+  const tds = document.querySelectorAll(".eleven-oclock, .twelve-oclock, .one-oclock, .center-tile");
+  tds.forEach(function (td) {
+    const value = parseInt(td.innerHTML);
+    td.innerHTML = value - 3;
+
+    if (td.innerHTML <= 1) {
+      td.classList.add("hidden");
+      td.innerHTML = "Timer: 1:40";
+
+      // Disable onClick
+      northMeteorButton.style.pointerEvents = "none";
+      var duration = 100000; // 100000 milliseconds = 1 minute and 40 seconds
+
+      td.timer = setInterval(function () {
+        duration -= 1000; // 1000 milliseconds = 1 second
+        var minutes = Math.floor(duration / 60000);
+        var seconds = Math.floor((duration % 60000) / 1000);
+        td.innerHTML = "Timer: " + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+        if (duration <= 0) {
+          td.classList.remove("hidden");
+
+          // re-enable onClick
+          northMeteorButton.style.pointerEvents = "auto";
+
+          // Check if the element has the class "center-tile"
+          if (td.classList.contains("center-tile")) {
+            td.innerHTML = "14";
+          } else {
+            td.innerHTML = "3";
+          }
+          td.style.border = "";
+          clearInterval(td.timer);
+        }
+      }, 1000); // 1000 milliseconds = 1 second
+    }
+  });
+});
+
+// ================== South Meteor ===============================================
+
+const southMeteorButton = document.querySelector(".south-meteor");
+southMeteorButton.addEventListener("click", function () {
+  const tds = document.querySelectorAll(".seven-oclock, .six-oclock, .five-oclock, .center-tile");
+  tds.forEach(function (td) {
+    const value = parseInt(td.innerHTML);
+    td.innerHTML = value - 3;
+
+    if (td.innerHTML <= 1) {
+      td.classList.add("hidden");
+      td.innerHTML = "Timer: 1:40";
+
+      // Disable onClick
+      southMeteorButton.style.pointerEvents = "none";
+      var duration = 100000; // 100000 milliseconds = 1 minute and 40 seconds
+
+      td.timer = setInterval(function () {
+        duration -= 1000; // 1000 milliseconds = 1 second
+        var minutes = Math.floor(duration / 60000);
+        var seconds = Math.floor((duration % 60000) / 1000);
+        td.innerHTML = "Timer: " + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+        if (duration <= 0) {
+          td.classList.remove("hidden");
+
+          // re-enable onClick
+          southMeteorButton.style.pointerEvents = "auto";
+
+          // Check if the element has the class "center-tile"
+          if (td.classList.contains("center-tile")) {
+            td.innerHTML = "14";
+          } else {
+            td.innerHTML = "3";
+          }
+          td.style.border = "";
+          clearInterval(td.timer);
+        }
+      }, 1000); // 1000 milliseconds = 1 second
+    }
+  });
+});
+
 // Function to subtract the td name by 1
 function subtract(element) {
   if (element.style.border !== "0") {
     var value = parseInt(element.innerHTML);
+
     if (value > 1) {
       element.innerHTML = value - 1;
       if (value == 1) {
